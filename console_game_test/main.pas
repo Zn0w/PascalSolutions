6c5cb5f3e36ef6fd3ccfa7d22c7e148babaafe39
Daniil Blagiy <blagiy.daniil@gmail.com>
2018-09-21 21:17:00 +0300

    little update

--- console_game_test/main.pas
+++ console_game_test/main.pas
@@ -1,10 +1,17 @@
 uses crt;
 
 var
-	map : array [0..9] of integer;
+	map : array [0..9] of char;
+	i : integer;
 
 begin
 
+	for i := 0 to 9 do
+		map[i] := '*';
+	
 	writeln('Hello World!');
+
+	for i := 0 to 9 do
+		writeln(map[i]);
 
 end.
