45d01b6165292f2296e91fb01d95839f12e9b505
Daniil Blagiy <blagiy.daniil@gmail.com>
2018-09-21 21:38:50 +0300

    added main loop

--- console_game_test/main.pas
+++ console_game_test/main.pas
@@ -3,6 +3,8 @@ uses crt;
 var
 	map : array [0..9, 0..9] of char;
 	i, j : integer;
+	terminate : boolean;
+	player_input : char;
 
 procedure init_map();
 begin
@@ -21,12 +23,27 @@ begin
 	end;
 end;
 
+procedure process_input();
+begin
+	if (player_input = 'y') then terminate := true;
+end;
+
 begin
 
 	init_map();
+	terminate := false;
 	
-	writeln('Hello World!');
+	while (not terminate) do
+	begin
+		display_map();
 
-	display_map();
+		write('Do you want to terminate? (y / n)  ');
+		readln(player_input);
+
+		process_input();
+
+		writeln();
+		writeln();
+	end;
 
 end.
