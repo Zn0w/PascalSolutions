840e947dae6734de6904786b19ea509d3b4b9040
Daniil Blagiy <blagiy.daniil@gmail.com>
2018-09-21 21:27:33 +0300

    put map init adn display in functions (procedures)

--- console_game_test/main.pas
+++ console_game_test/main.pas
@@ -4,19 +4,29 @@ var
 	map : array [0..9, 0..9] of char;
 	i, j : integer;
 
+procedure init_map();
 begin
-
 	for i := 0 to 9 do
 		for j := 0 to 9 do
 			map[i, j] := '*';
-	
-	writeln('Hello World!');
+end;
 
+procedure display_map();
+begin
 	for i := 0 to 9 do
 	begin
 		for j := 0 to 9 do
 			write(map[i, j], ' ');
 		writeln();
 	end;
+end;
+
+begin
+
+	init_map();
+	
+	writeln('Hello World!');
+
+	display_map();
 
 end.
