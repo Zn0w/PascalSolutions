4d94905a1329fc6815446357cf7234b77cc8be98
Daniil Blagiy <blagiy.daniil@gmail.com>
2018-09-21 22:21:38 +0300

    added player running on the screen

--- console_game_test/main.pas
+++ console_game_test/main.pas
@@ -1,10 +1,16 @@
 uses crt;
 
+type
+	Player = record
+		x, y : integer;
+	end;
+
 var
 	map : array [0..9, 0..9] of char;
 	i, j : integer;
 	terminate : boolean;
 	player_input : char;
+	player1 : Player;
 
 procedure init_map();
 begin
@@ -18,13 +24,22 @@ begin
 	for i := 0 to 9 do
 	begin
 		for j := 0 to 9 do
-			write(map[i, j], ' ');
+			if (i = player1.y) and (j = player1.x) then
+				write('o ')
+			else
+				write(map[i, j], ' ');
 		writeln();
 	end;
 end;
 
 procedure process_input();
 begin
+	if (player_input = 'r') then player1.x := player1.x + 1
+	else if (player_input = 'l') then player1.x := player1.x - 1
+	else if (player_input = 'u') then player1.y := player1.y - 1
+	else if (player_input = 'd') then player1.y := player1.y + 1
+	else terminate := player_input = 'y';
+	
 	if (player_input = 'y') then terminate := true;
 end;
 
@@ -32,9 +47,12 @@ begin
 
 	init_map();
 	terminate := false;
+	player1.x := 5;
+	player1.y := 5;
 	
 	while (not terminate) do
 	begin
+		clrscr();
 		display_map();
 
 		write('Do you want to terminate? (y / n)  ');
